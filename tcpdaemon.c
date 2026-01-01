7383a3e8d8d879ff02724a8d118d8149c2bb4bf6
RensoPeralta25 <ripu0001@ce.pucmm.edu.do>
2026-01-01 13:25:35 -0400

    aplicando socket

--- tcpdaemon.c
+++ tcpdaemon.c
@@ -5,11 +5,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #define LOG_FILE "/var/log/tcpdaemon.log" // ruta del archivo .log
 #define TRUE 1
+#define PORT 8080
 
 FILE *log_file; //archivo que apunta a tcpdaemon.log
+int server_fd;
 
 // Funcion que abre y escribe en el .log
 
@@ -63,6 +69,11 @@ void convertir_en_daemon(){
 
 int main(){
 
+	struct sockaddr_in address; // Estructura que define familia de protocolos IPv4 para comunicacion.
+	socklen_t addrlen = sizeof(address); // Longitud de la direccion ip
+
+	convertir_en_daemon();
+
 	log_file = fopen(LOG_FILE, "a");
 
 	if (!log_file){
@@ -75,11 +86,53 @@ int main(){
 	signal(SIGTERM, manejador);
 	signal(SIGINT, manejador);
 
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0){
+		escribir_log("Error al crear socket");
+		exit(EXIT_FAILURE);
+	}
+
+	int opt = 1;
+	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+
+		escribir_log("Error en bind()");
+		exit(EXIT_FAILURE);
+
+	}
+
+	if (listen(server_fd, 10) < 0){
+
+		escribir_log("Error en bind()");
+		exit(EXIT_FAILURE);
+
+	}
+
+	char msg[128];
+
+	snprintf(msg, sizeof(msg), "Servidor TCP escuchando en el puerto 8080");
+
+	escribir_log(msg);
+
 	// Bucle
 	while (TRUE){
 
-		sleep(5);
-		escribir_log("Daemon activo...");
+		int client_fd = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+
+		if (client_fd < 0){
+			escribir_log("Error en accept()");
+			continue;
+		}
+
+		char msg[] = "Conexion aceptada por tcpdaemon\n";
+		send(client_fd, msg, strlen(msg), 0);
+
+		close(client_fd);
 	}
 
 	return 0;
